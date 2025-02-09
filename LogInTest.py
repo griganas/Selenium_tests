cdcf9339971821a3408a30a27e1d73471861d977
griganas <anastasiagk5@yahoo.gr>
2025-02-09 13:15:13 +0200

    wrong username / password

--- LogInTest.py
+++ LogInTest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 import time
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions, wait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 options = webdriver.ChromeOptions()
@@ -34,6 +34,6 @@ driver.find_element(By.CSS_SELECTOR,"span.text-white.termsText").click()
 time.sleep(10)
 driver.find_element(By.ID, "signInBtn").click()
 wait = WebDriverWait(driver,10)
-wait.until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, ".alert-danger")))
+wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".alert-danger")))
 print(driver.find_element(By.CSS_SELECTOR, ".alert-danger").text)
 
